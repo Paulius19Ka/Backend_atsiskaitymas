e9ad7b662b5ff829f7e57f9e198afff3aea686ad
Paulius19Ka <kallabow2@gmail.com>
2025-06-02 09:26:45 +0300

    find specific book route, basic sort/limit/skip

--- server/controllers/booksController.js
+++ server/controllers/booksController.js
@@ -1,9 +1,21 @@
 import { connectToDB } from "./helper.js";
 
+const dynamicQuery = () => {
+  const settings = {
+    filter: {},
+    sort: { title: 1 },
+    skip: 0,
+    limit: 10
+  };
+
+  return settings;
+};
+
 const getAllBooks = async (req, res) => {
   const client = await connectToDB();
   try{
-    const DB_RESPONSE = await client.db('Library').collection('books').find().sort().toArray();
+    const settings = dynamicQuery();
+    const DB_RESPONSE = await client.db('Library').collection('books').find().sort(settings.sort).skip(settings.skip).limit(settings.limit).toArray();
     res.send(DB_RESPONSE);
   } catch(err){
     console.log(err);
@@ -13,4 +25,19 @@ const getAllBooks = async (req, res) => {
   }
 };
 
-export { getAllBooks };
+const getSpecificBook = async (req, res) => {
+  const { id } = req.params;
+  const client = await connectToDB();
+  let filter = { _id: id };
+  try{
+    const DB_RESPONSE = await client.db('Library').collection('books').findOne(filter);
+    res.send(DB_RESPONSE);
+  } catch(err){
+    console.log(err);
+    res.status(500).send({ error: err, message: `Something went wrong with the server.` });
+  } finally{
+    await client.close();
+  }
+};
+
+export { getAllBooks, getSpecificBook };
--- server/routes/booksRoutes.js
+++ server/routes/booksRoutes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 
-import { getAllBooks } from "../controllers/booksController.js"
+import { getAllBooks, getSpecificBook } from "../controllers/booksController.js"
 
 const router = Router();
 
 router.get('', getAllBooks);
+router.get('/:id', getSpecificBook);
 
 export default router;
