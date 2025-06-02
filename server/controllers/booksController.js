c8904ba61c952a47d80a367c34db70ad0b4885cc
Paulius19Ka <kallabow2@gmail.com>
2025-06-02 09:38:45 +0300

    installed uuid, checking if id is a valid uuid

--- server/controllers/booksController.js
+++ server/controllers/booksController.js
@@ -1,3 +1,5 @@
+import { validate as uuidValidate } from 'uuid';
+
 import { connectToDB } from "./helper.js";
 
 const dynamicQuery = () => {
@@ -28,9 +30,18 @@ const getAllBooks = async (req, res) => {
 const getSpecificBook = async (req, res) => {
   const { id } = req.params;
   const client = await connectToDB();
+
+  // check if id is a valid uuid
+  if(!uuidValidate(id)){
+    return res.status(400).send({ error: `[${id}] is not a valid id. The id must be a valid uuid.` })
+  }
+
   let filter = { _id: id };
   try{
     const DB_RESPONSE = await client.db('Library').collection('books').findOne(filter);
+    if(!DB_RESPONSE){
+      return res.status(404).send({ error: `Book [id: ${id}] was not found.`});
+    };
     res.send(DB_RESPONSE);
   } catch(err){
     console.log(err);
