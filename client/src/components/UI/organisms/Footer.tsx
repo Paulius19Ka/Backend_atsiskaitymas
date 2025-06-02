06ee8f9a9915504c1f71dfc94db4850640525614
Paulius19Ka <kallabow2@gmail.com>
2025-06-02 17:04:04 +0300

    home page, footer

--- client/src/components/UI/organisms/Footer.tsx
+++ client/src/components/UI/organisms/Footer.tsx
@@ -3,16 +3,37 @@ import styled from "styled-components";
 const StyledFooter = styled.footer`
   height: 40px;
   background-color: black;
+  padding: 0px 5px;
 
   display: flex;
-  justify-content: center;
+  justify-content: space-between;
   align-items: center;
+
+  > div{
+    display: flex;
+    gap: 10px;
+  }
 `;
 
 const Footer = () => {
   return (
     <StyledFooter>
-      <span>Copyright &copy; Paulius Karbauskas {new Date().getFullYear()}</span>
+      <span>Copyright &copy; PK {new Date().getFullYear()}</span>
+      <div>
+        <span>
+          98 Withers Close, Allerton, L18 4UN, UK
+        </span>
+        <span>
+          Mon-Fri: 8 AM - 4 PM
+          Sat-Sun: Closed
+        </span>
+      </div>
+      <div>
+        <span>bsky</span>
+        <span>FB</span>
+        <span>IG</span>
+        <span>Twitter</span>
+      </div>
     </StyledFooter>
   );
 }
