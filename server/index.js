3ee7bb9cb839254d995d6f697ec6245602d37d36
Paulius19Ka <kallabow2@gmail.com>
2025-06-02 09:41:18 +0300

    added cors options

--- server/index.js
+++ server/index.js
@@ -5,11 +5,14 @@ import 'dotenv/config';
 import booksRoutes from './routes/booksRoutes.js'
 
 const PORT = process.env.PORT || 5501;
+const corsOptions = {
+  origin: "http://localhost:5173"
+};
 
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors(corsOptions));
 
 app.listen(PORT, () => {
   console.log(`Server is running on PORT: ${PORT}`);
