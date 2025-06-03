7cfc2a0b8b275833659e5bd7ae0e8a2815b705ed
Paulius19Ka <kallabow2@gmail.com>
2025-06-03 09:28:58 +0300

    main style

--- client/src/components/outlets/MainOutlet.tsx
+++ client/src/components/outlets/MainOutlet.tsx
@@ -4,10 +4,22 @@ import Header from "../UI/organisms/Header";
 import Footer from "../UI/organisms/Footer";
 
 const StyledMain = styled.main`
-  min-height: calc(100vh - 100px - 40px);
+  min-height: calc(100vh - 40px - 40px);
+  background-color: #373043;
 
   h2{
     margin: 0;
+  }
+
+  > section{
+    max-width: 80%;
+    margin: 0 auto;
+    padding: 20px;
+
+    > p{
+      margin: 0;
+      padding: 10px 0px;
+    }
   }
 `;
 
